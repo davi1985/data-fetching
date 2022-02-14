71acfe7a8ed0c7c688fd0c9dcae4c2be97620b1c
Davi Silva <davisilvaphoto@gmail.com>
2022-02-14 15:35:19 -0300

    update data with react query

--- src/pages/Repo.tsx
+++ src/pages/Repo.tsx
@@ -1,6 +1,11 @@
 import { useQueryClient } from "react-query";
 import { useParams } from "react-router-dom";
 
+type Repository = {
+  full_name: string;
+  description: string;
+};
+
 export function Repo() {
   const params = useParams();
   const currentRepository = params["*"] as string;
@@ -8,7 +13,21 @@ export function Repo() {
   const queryClient = useQueryClient();
 
   async function handleChangeRepositoryDescription() {
-    await queryClient.invalidateQueries(["repos"]);
+    // call API to update repo description
+
+    const previousRepos = queryClient.getQueryData<Repository[]>("repos");
+
+    if (previousRepos) {
+      const nextRepos = previousRepos.map((repo) => {
+        if (repo.full_name === currentRepository) {
+          return { ...repo, description: "Testando update" };
+        } else {
+          return repo;
+        }
+      });
+
+      queryClient.setQueriesData("repos", nextRepos);
+    }
   }
 
   return (
